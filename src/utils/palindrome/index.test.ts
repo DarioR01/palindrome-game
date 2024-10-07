a1aa0308c05c8419795ccc7ea4cadb6120221204
DarioR01 <dario.russo.052001@gmail.com>
2024-10-07 12:41:06 +0100

    feat: palindrome util function unit tests

---
+++ src/utils/palindrome/index.test.ts
@@ -0,0 +1,60 @@
+import { isPalindrome, scorePalindrome } from ".";
+
+const PALINDROMES = [
+  { word: "racecar", score: 7 },
+  { word: "level", score: 5 },
+  { word: "madam", score: 5 },
+  { word: "A man a plan a canal Panama", score: 21 },
+  { word: "Was it a car or a cat I saw", score: 21 },
+  { word: "No lemon no melon", score: 15 },
+  { word: "12321", score: 5 },
+];
+
+const NON_PALINDROMES = [
+  { word: "apple", score: 5 },
+  { word: "banana", score: 6 },
+  { word: "orange", score: 6 },
+  { word: "Hello world", score: 10 },
+  { word: "JavaScript is fun", score: 15 },
+  { word: "I love coding", score: 11 },
+  { word: "452", score: 3 },
+];
+
+const NON_PALINDROME_SCORE = 0;
+
+describe("Utils - isPalindrome", () => {
+  PALINDROMES.forEach((palindrome) => {
+    test(`should correctly identify palindromes: ${palindrome.word}`, () => {
+      expect(isPalindrome(palindrome.word)).toBeTruthy;
+    });
+  });
+
+  NON_PALINDROMES.forEach((nonPalindrome) => {
+    test(`should correctly identify non-palindromes: ${nonPalindrome.word}`, () => {
+      expect(isPalindrome(nonPalindrome.word)).toBeFalsy();
+    });
+  });
+
+  it("should correctly identify that an empty string is not a palindrome", () => {
+    const TEST_WORD = " ";
+    expect(isPalindrome(TEST_WORD)).toBeFalsy();
+  });
+
+  it("should correctly identify that a single letter string is not a palindrome", () => {
+    const TEST_WORD = "a";
+    expect(isPalindrome(TEST_WORD)).toBeFalsy();
+  });
+});
+
+describe("Utils - scorePalindrome", () => {
+  PALINDROMES.forEach((palindrome) => {
+    test(`should correctly score palindromes based on number of characters: ${palindrome.word}`, () => {
+      expect(scorePalindrome(palindrome.word)).toBe(palindrome.score);
+    });
+  });
+
+  it("should correctly score a non palindrome word", () => {
+    const TEST_WORD = "test";
+    expect(scorePalindrome(TEST_WORD)).toBe(NON_PALINDROME_SCORE);
+  });
+});
---
+++ src/utils/palindrome/index.ts
@@ -0,0 +1,7 @@
+export const isPalindrome = (word: string): boolean => {
+  return false;
+};
+
+export const scorePalindrome = (word: string): number => {
+  return 0;
+};
